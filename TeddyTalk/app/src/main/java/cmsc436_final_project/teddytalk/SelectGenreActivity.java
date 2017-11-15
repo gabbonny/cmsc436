35eb363dc5f84d0444c5665425a5e41325c172ec
Marc Edry Hipona <mhipona@umd.edu>
2017-11-15 13:19:44 -0500

    Started Genre Select Activity

---
+++ TeddyTalk/app/src/main/java/cmsc436_final_project/teddytalk/SelectGenreActivity.java
@@ -0,0 +1,13 @@
+package cmsc436_final_project.teddytalk;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SelectGenreActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_select_genre);
+    }
+}
