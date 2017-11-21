3dc995f232bde19d27d555eefc277ff8803a1a6b
jonellebowen <jonellebowen@yahoo.com>
2017-11-21 11:11:22 -0500

    Adding content..(Jonelle)

--- TeddyTalk/app/src/main/java/cmsc436_final_project/teddytalk/MainActivity.java
+++ TeddyTalk/app/src/main/java/cmsc436_final_project/teddytalk/MainActivity.java
@@ -2,6 +2,9 @@ package cmsc436_final_project.teddytalk;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        ImageButton implicitActivationButton = (ImageButton) findViewById(R.id.start_button);
+        implicitActivationButton.setOnClickListener(new View.OnClickListener() {
+
+            // Call startImplicitActivation() when pressed
+            @Override
+            public void onClick(View v) {
+
+                // start genre activity
+
+                Toast.makeText(MainActivity.this,
+                        "START!", Toast.LENGTH_SHORT).show();
+
+            }
+        });
     }
 }
