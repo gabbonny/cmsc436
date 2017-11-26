fb159a00fc3bfefb78717d6a87e10747349c0d5d
jonellebowen <jonellebowen@yahoo.com>
2017-11-25 20:49:09 -0500

    made changes (Jo)

--- TeddyTalk/app/src/main/java/cmsc436_final_project/teddytalk/MainActivity.java
+++ TeddyTalk/app/src/main/java/cmsc436_final_project/teddytalk/MainActivity.java
@@ -9,6 +9,11 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.Toast;
+import android.content.Intent;
+import android.view.animation.ScaleAnimation;
+import android.view.animation.Animation;
+
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -27,22 +32,62 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //setUpSound(); // set up sound stuff
+
         ColorDrawable colorDraw = new ColorDrawable(Color.parseColor("#882472"));//#20a780
 
         getSupportActionBar().setBackgroundDrawable(colorDraw);
         ImageButton implicitActivationButton = (ImageButton) findViewById(R.id.start_button);
         implicitActivationButton.setOnClickListener(new View.OnClickListener() {
 
-            // Call startImplicitActivation() when pressed
+            Animation anim = new ScaleAnimation(
+                    1f, 1f, // Start and end values for the X axis scaling
+                    startScale, endScale, // Start and end values for the Y axis scaling
+                    Animation.RELATIVE_TO_SELF, 0f, // Pivot point of X scaling
+                    Animation.RELATIVE_TO_SELF, 1f); // Pivot point of Y scaling
+    anim.setFillAfter(true); // Needed to keep the result of the animation
+    anim.setDuration(1000);
+    v.startAnimation(anim);
+
             @Override
             public void onClick(View v) {
 
                 // start genre activity
+                Intent selectGenreIntent = new Intent(MainActivity.this, SelectGenreActivity.class);
 
-                Toast.makeText(MainActivity.this,
-                        "START!", Toast.LENGTH_SHORT).show();
+                mSoundPool.play(mSoundID, mStreamVolume,
+                        mStreamVolume, 1, 0, 1.0f);
+                startActivity(selectGenreIntent);
+//                Toast.makeText(MainActivity.this,
+//                        "START!", Toast.LENGTH_SHORT).show();
 
             }
         });
     }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        mAudioManager = (AudioManager) getSystemService(AUDIO_SERVICE);
+
+        mStreamVolume = (float) mAudioManager
+                .getStreamVolume(AudioManager.STREAM_MUSIC)
+                / mAudioManager.getStreamMaxVolume(AudioManager.STREAM_MUSIC);
+
+        mSoundPool = new SoundPool(10, AudioManager.STREAM_MUSIC, 0);
+
+        mSoundID = mSoundPool.load(this, R.raw.bubble_pop, 1);
+
+    }
+
+    @Override
+    protected void onPause() {
+        mSoundPool.unload(mSoundID);
+        mSoundPool.release();
+        mSoundPool = null;
+        super.onPause();
+    }
+
 }
