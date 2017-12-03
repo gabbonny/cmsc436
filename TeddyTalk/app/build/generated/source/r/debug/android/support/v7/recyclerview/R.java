9a4432a3b8d1238888990caff4d2e67d982066c0
stemooreUMD <stemoore@terpmail.umd.edu>
2017-12-03 18:34:37 -0500

    redo playback design

--- TeddyTalk/app/build/generated/source/r/debug/android/support/v7/recyclerview/R.java
+++ TeddyTalk/app/build/generated/source/r/debug/android/support/v7/recyclerview/R.java
@@ -99,12 +99,12 @@ public final class R {
         public static final int notification_background = 0x7f080077;
         public static final int notification_main_column = 0x7f080078;
         public static final int notification_main_column_container = 0x7f080079;
-        public static final int right_icon = 0x7f080092;
-        public static final int right_side = 0x7f080093;
-        public static final int text = 0x7f0800c6;
-        public static final int text2 = 0x7f0800c7;
-        public static final int time = 0x7f0800d2;
-        public static final int title = 0x7f0800d3;
+        public static final int right_icon = 0x7f080093;
+        public static final int right_side = 0x7f080094;
+        public static final int text = 0x7f0800c8;
+        public static final int text2 = 0x7f0800c9;
+        public static final int time = 0x7f0800d4;
+        public static final int title = 0x7f0800d5;
     }
     public static final class integer {
         public static final int status_bar_notification_info_maxnum = 0x7f090009;
