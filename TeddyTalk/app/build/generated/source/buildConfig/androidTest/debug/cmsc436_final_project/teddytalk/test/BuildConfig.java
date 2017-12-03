7990630bb570d31d2412c282272d04e8d68a9d1d
rstudio <rstudio@example.com>
2017-12-03 15:08:23 -0500

    Added Drawables for ChangeAvatar activity

---
+++ TeddyTalk/app/build/generated/source/buildConfig/androidTest/debug/cmsc436_final_project/teddytalk/test/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package cmsc436_final_project.teddytalk.test;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "cmsc436_final_project.teddytalk.test";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
